c9890dbabdade153bdc6b98d2d4f7e248066ce00
hoyinli1211 <hoyinli.r@gmail.com>
2023-02-04 12:11:19 +0800

    Create App.py

---
+++ App.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from faker import Faker
+import pandas as pd
+import random
+
+#Sidebar
+st.sidebar.title("Instructions:")
+st.sidebar.markdown("1. ")
+st.sidebar.markdown("2. ")
+st.sidebar.markdown("3. ")
+st.sidebar.markdown("4. ")
+
+#Main Page
+st.title("Formula One Studio")
+tabs = st.tabs(["Note","Main Page"])
